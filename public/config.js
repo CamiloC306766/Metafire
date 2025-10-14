7d37d8275f2e48499fa6b9af0ac17370572db527
Camilo <camiloc306766@gmail.com>
2025-10-14 16:52:08 -0500

    Implementacion variables de entorno

---
+++ public/config.js
@@ -0,0 +1,9 @@
+window.env = {
+  apiKey: "AIzaSyBY3oE4tQb_3_45-AYzINWN1YaI2ypnUuE",
+  authDomain: "integracionmetafirebase.firebaseapp.com",
+  projectId: "integracionmetafirebase",
+  storageBucket: "integracionmetafirebase.firebasestorage.app",
+  messagingSenderId: "671107320019",
+  appId: "1:671107320019:web:3c5d3fe6592c7d5f48ec26",
+  measurementId: "G-QL1PSJCCVH"
+};
